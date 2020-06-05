02123b19524952029b5460bbaad259eeb206f564
Bryan Fisher <brf2010@med.cornell.edu>
2020-06-05 13:13:05 -0400

    use time range from config

--- bin/cert-manager.py
+++ bin/cert-manager.py
@@ -5,6 +5,7 @@ from pprint import pprint
 import os
 import sys
 import ConfigParser
+from datetime import datetime, timedelta
 
 # get application path
 APP_LOCATION = os.path.dirname(os.path.dirname(sys.argv[0]))
@@ -18,6 +19,7 @@ try:
     customerUri = config.get("cert-manager", "customerUri")
     login = config.get("cert-manager", "login")
     password = config.get("cert-manager", "password")
+    time_range = config.get("cert-manager", "time_range")
 except:
     sys.exit("values missing from cert-manager.conf")  
 
@@ -28,14 +30,22 @@ uri = "https://cert-manager.com/api/report/v1/ssl-certificates"
 # password = getpass()
 # customerUri = "redacted"
 
+# calculate start and end times for certificate retrieval
+time_format = "%Y-%m-%dT%H:%M:%S.000Z"
+now = datetime.utcnow()
+start_time = now.strftime(time_format)
+end_datetime = now + timedelta(days=time_range)
+end_time = end_datetime.strftime(time_format)
+
+
 # authentication headers
 headers = {"login":login,
            "password":password,
            "customerUri": customerUri}
 
 # report specification
-report_data = {"from":"2020-06-02T00:00:00.000Z",
-                "to":"2020-06-09T00:00:00.000Z",
+report_data = {"from":start_time,
+                "to":end_time,
                 "organizationIds":[],
                 "certificateStatus":0, # any
                 #"certificateRequestSource":[], # Client Admin
@@ -45,9 +55,8 @@ report_data = {"from":"2020-06-02T00:00:00.000Z",
 # get our report
 r = requests.post(uri, headers=headers, json=report_data)
 
-print(r.text)
-report = r.json()['reports']
 #print(r.text)
+report = r.json()['reports']
 
 for certificate in report:
     pprint(certificate)
